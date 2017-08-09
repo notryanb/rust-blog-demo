52536cc175ac56b99c341ce3f53018ab5800393a
Ryan B <notryanb@gmail.com>
2017-08-09 11:43:20 -0400

    Add support for Nullable field, published_at

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
 use schema::posts;
+use std::time::SystemTime;
 
 #[derive(Queryable, Serialize)]
 pub struct Post {
@@ -6,11 +7,24 @@ pub struct Post {
     pub title: String,
     pub body: String,
     pub published: bool,
+    pub published_at: Option<SystemTime>
 }
 
-#[derive(Insertable)]
+// #[derive(Identifiable, Serialize)]
+// #[table_name="posts"]
+// pub struct UpdatePost {
+//     pub id: i32,
+//     pub title: String,
+//     pub body: String,
+//     pub published: bool,
+//     pub publish_at: SystemTime
+// }
+
+#[derive(AsChangeset, Insertable)]
 #[table_name="posts"]
+#[changeset_options(treat_none_as_null="true")]
 pub struct NewPost<'a> {
     pub title: &'a str,
-    pub body: &'a str
+    pub body: &'a str,
+    pub published_at: Option<SystemTime>
 }
