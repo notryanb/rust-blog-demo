96e9939fca624ea028118317835c7d087998a5f0
Ryan B <notryanb@gmail.com>
2017-08-31 16:15:51 -0400

    Add very basic and crude auth that creates a cookie

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -7,6 +7,7 @@ use rocket_contrib::Template;
 use rocket::http::{Cookie, Cookies};
 use rocket::response::Redirect;
 use rocket::request::Form;
+use serde_json;
 use tera::Context;
 
 use super::DbConn;
@@ -23,8 +24,50 @@ fn login() -> Template {
     Template::render("auth/login", &context)
 }
 
+#[post("/login", data="<form>")]
+fn authenticate(form: Form<LoginForm>, mut cookies: Cookies, conn: DbConn) -> Redirect {
+    use super::schema::users::dsl::*;
+
+    // Need to validate a few things
+    //      1. Find user || error semantically
+    //      2. Invalid pw || error semantically
+    //      3. Already logged in, redirect. (also, don't allow in the UI)
+    //
+    //  * Think about User states. Need AuthenticatedUser?
+    //  * Make illegal states unrepresentable....aka, UnAuthenticatedUser..?
+    //  * Think about serialization...The password.. even hashed shouldn't be
+    //      available to the client at all. Need to custom implement Serialize 
+    //      trait for user?
+    
+    let form = form.get();
+    let found_users = users.filter(email.eq(&form.email))
+        .limit(1)
+        .load::<User>(&*conn)
+        .expect("Didn't find any users");
+
+    let found_user = &found_users[0];
+
+    if bcrypt::verify(&form.password, &found_user.password).unwrap() {
+        let sessions = serde_json::to_string(&found_user);
+
+        if sessions.is_ok() {
+            let cookie = Cookie::build("sessions_auth".to_owned(), sessions.unwrap())
+                .path("/")
+                .finish();
+
+            cookies.add_private(cookie);
+        }
+    }
+
+    Redirect::to("/")
+}
+
+// TODO: Logout Path.
+//
+// TODO: Register Path.
+
 pub fn routes() -> Vec<rocket::Route> {
-    routes![login]
+    routes![authenticate, login]
 }
 
 
