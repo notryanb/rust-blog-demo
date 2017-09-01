f819e3c92cfe5fa39f25013271a77f88a74107db
Ryan B <notryanb@gmail.com>
2017-09-01 15:34:58 -0400

    Add Authenticated and AnonymousUser models. Serialize User by hand to add login state

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,6 +1,6 @@
 use bcrypt;
-use bcrypt::{DEFAULT_COST, hash};
-use diesel;
+// use bcrypt::{DEFAULT_COST, hash};
+// use diesel;
 use diesel::prelude::*;
 use rocket;
 use rocket_contrib::Template;
@@ -18,44 +18,54 @@ mod forms;
 pub mod models;
 
 #[get("/login")]
-fn login() -> Template {
-    let context = Context::new();
+fn login(user: AnonymousUser) -> Template {
+    let mut context = Context::new();
+
+    context.add("user", &user);
 
     Template::render("auth/login", &context)
 }
 
 #[post("/login", data="<form>")]
-fn authenticate(form: Form<LoginForm>, mut cookies: Cookies, conn: DbConn) -> Redirect {
+fn authenticate(
+        user: AnonymousUser,
+        form: Form<LoginForm>, 
+        mut cookies: Cookies,
+        conn: DbConn
+    ) -> Redirect {
     use super::schema::users::dsl::*;
 
     // Need to validate a few things
-    //      1. Find user || error semantically
-    //      2. Invalid pw || error semantically
+    //      1. Find user || error semantically (Flash messages from Rocket?)
+    //      2. Invalid pw || error semantically (Flash messages from Rocket?)
     //      3. Already logged in, redirect. (also, don't allow in the UI)
     //
     //  * Think about User states. Need AuthenticatedUser?
+    //  * In either error case, just report user or password not found
     //  * Make illegal states unrepresentable....aka, UnAuthenticatedUser..?
     //  * Think about serialization...The password.. even hashed shouldn't be
     //      available to the client at all. Need to custom implement Serialize 
     //      trait for user?
-    
-    let form = form.get();
-    let found_users = users.filter(email.eq(&form.email))
-        .limit(1)
-        .load::<User>(&*conn)
-        .expect("Didn't find any users");
+   
+    if cookies.get("sessions_auth").is_none() {
+        let form = form.get();
+        let found_users = users.filter(email.eq(&form.email))
+            .limit(1)
+            .load::<User>(&*conn)
+            .expect("Didn't find any users");
 
-    let found_user = &found_users[0];
+        let found_user = &found_users[0];
 
-    if bcrypt::verify(&form.password, &found_user.password).unwrap() {
-        let sessions = serde_json::to_string(&found_user);
+        if bcrypt::verify(&form.password, &found_user.password).unwrap() {
+            let sessions = serde_json::to_string(&found_user);
 
-        if sessions.is_ok() {
-            let cookie = Cookie::build("sessions_auth".to_owned(), sessions.unwrap())
-                .path("/")
-                .finish();
+            if sessions.is_ok() {
+                let cookie = Cookie::build("sessions_auth".to_owned(), sessions.unwrap())
+                    .path("/")
+                    .finish();
 
-            cookies.add_private(cookie);
+                cookies.add_private(cookie);
+            }
         }
     }
 
--- src/auth/models.rs
+++ src/auth/models.rs
@@ -1,12 +1,21 @@
+// Influenced heavily from
+// https://git.bananium.fr/eijebong/bananium.rs/blob/master/src/auth/models.rs
+// Look into refactoring...
+
 use rocket::outcome::Outcome::*;
 use rocket::http::Status;
 use rocket::request::{FromRequest, Request, Outcome};
-// use serde_json;
-// use serde::ser;
-// use serde::ser::SerializeStruct;
+use serde_json;
+use serde::ser;
+use serde::ser::SerializeStruct;
 use schema::users;
 
-#[derive(Debug, Identifiable, Queryable, Serialize, Deserialize)]
+// Custom User serialization? We don't want the password being exposed anywhere in the code that it
+// doesn't need to be.
+// Also, We can encode another field that checks if the id == AnonymousUser's ID, which will
+// give us the state of an authenticated user or anonymous user
+
+#[derive(Debug, Identifiable, Queryable, Deserialize)]
 pub struct User {
     pub id: i32,
     pub first_name: String,
@@ -31,24 +40,91 @@ pub struct UpdateUser<'a> {
     pub email: &'a str
 }
 
-// impl<'a, 'r> FromRequest<'a, 'r> for User{
-//     type Error = ();
+impl ser::Serialize for User {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        let mut s = serializer.serialize_struct("User", 6)?;
+        s.serialize_field("id", &self.id)?;
+        s.serialize_field("first_name", &self.id)?;
+        s.serialize_field("last_name", &self.id)?;
+        s.serialize_field("email", &self.id)?;
+        s.serialize_field("is_anonymous", &(self.id == -1))?;
+        s.end()
+    }
+}
 
-//     fn from_request(request: &'a Request<'r>) -> Outcome<User, ()> {
-//         // Grab cookies
-//         let request = request.clone();
-//         let mut cookies = request.cookies();
+impl<'a, 'r> FromRequest<'a, 'r> for User{
+    type Error = ();
 
-//         if let Some(cookie) = cookies.get_private("session") {
-//             if let user: Result<User, _> = serde_json::from_str(cookie.value());
-//             if user.is_ok() {
-//                 return Success(user.unwrap());
-//             }
-//         }
+    fn from_request(request: &'a Request<'r>) -> Outcome<User, ()> {
+        let request = request.clone();
+        let mut cookies = request.cookies();
 
-//         Success(User {
-//             id:
-//         })
-//     }
-// }
+        if let Some(cookie) = cookies.get_private("session_auth") {
+            let user: Result<User, _> = serde_json::from_str(cookie.value());
+            if user.is_ok() {
+                return Success(user.unwrap());
+            }
+        }
 
+        Success(User {
+            id: -1,
+            first_name: "".to_owned(),
+            last_name: "".to_owned(),
+            email: "".to_owned(),
+            password: "".to_owned(),
+        })
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AuthenticatedUser(pub User);
+
+impl<'a, 'r> FromRequest<'a, 'r> for AuthenticatedUser{
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> Outcome<AuthenticatedUser, ()> {
+        let request = request.clone();
+        let mut cookies = request.cookies();
+
+        if let Some(cookie) = cookies.get_private("session_auth") {
+            let user: Result<User, _> = serde_json::from_str(cookie.value());
+            if user.is_err() {
+                return Failure((Status::raw(600), ()));
+            }
+
+            return Success(AuthenticatedUser(user.unwrap()));
+        }
+
+        Failure((Status::raw(600), ()))
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AnonymousUser(pub User);
+
+impl<'a, 'r> FromRequest<'a, 'r> for AnonymousUser{
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> Outcome<AnonymousUser, ()> {
+        let request = request.clone();
+        let mut cookies = request.cookies();
+
+        if let Some(cookie) = cookies.get_private("session_auth") {
+            let user: Result<User, _> = serde_json::from_str(cookie.value());
+            if user.is_ok() {
+                return Failure((Status::raw(601), ()));
+            }
+        }
+
+        Success(AnonymousUser(User {
+            id: -1,
+            first_name: "".to_owned(),
+            last_name: "".to_owned(),
+            email: "".to_owned(),
+            password: "".to_owned(),
+        }))
+    }
+}
