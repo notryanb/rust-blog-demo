8ffde866c0a4c7fefab252302eefd857cbd18af0
Ryan B <notryanb@gmail.com>
2017-09-13 09:46:36 -0400

    add register form struct

--- src/auth/forms.rs
+++ src/auth/forms.rs
@@ -3,3 +3,13 @@ pub struct LoginForm {
     pub email: String,
     pub password: String,
 }
+
+#[derive(Debug, FromForm, Serialize)]
+pub struct RegisterForm {
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub password: String,
+    pub password_confirm: String,
+}
+
